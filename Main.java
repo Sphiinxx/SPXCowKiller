6aeafc8335199b4530a7c7d241fec532c3cc55af
Matt <deathbyman33@yahoo.com>
2015-12-31 02:04:47 -0800

    Version 0.1

--- Main.java
+++ Main.java
@@ -2,10 +2,13 @@ package scripts.SPXCowKiller;
 
 import org.tribot.api.General;
 import org.tribot.api.Timing;
+import org.tribot.api.input.Mouse;
 import org.tribot.api2007.Login;
 import org.tribot.api2007.Skills;
 import org.tribot.script.Script;
 import org.tribot.script.ScriptManifest;
+import org.tribot.script.interfaces.MousePainting;
+import org.tribot.script.interfaces.MouseSplinePainting;
 import org.tribot.script.interfaces.Painting;
 import scripts.SPXCowKiller.api.Node;
 import scripts.SPXCowKiller.nodes.*;
@@ -18,7 +21,7 @@ import java.util.Collections;
  * Created by Sphiinx on 12/21/2015.
  */
 @ScriptManifest(authors = "Sphiinx", category = "Combat", name = "[SPX] Cow Killer", version = 0.1)
-public class Main extends Script implements Painting {
+public class Main extends Script implements Painting, MousePainting, MouseSplinePainting {
 
     private Variables variables = new Variables();
     private ArrayList<Node> nodes = new ArrayList<>();
@@ -98,6 +101,28 @@ public class Main extends Script implements Painting {
             g.drawString("Status: " + variables.status, 11, 312);
             g.drawString("v" + variables.version, 205, 330);
         }
+    }
+
+    @Override
+    public void paintMouse(Graphics graphics, Point point, Point point1) {
+        graphics.setColor(Color.BLACK);
+        graphics.drawRect(Mouse.getPos().x - 13, Mouse.getPos().y - 13, 27, 27); // Square rectangle Stroke
+        graphics.drawRect(Mouse.getPos().x, Mouse.getPos().y - 512, 1, 500); // Top y axis Line Stroke
+        graphics.drawRect(Mouse.getPos().x, Mouse.getPos().y + 13, 1, 500); // Bottom y axis Line Stroke
+        graphics.drawRect(Mouse.getPos().x + 13, Mouse.getPos().y, 800, 1); // Right x axis line Stroke
+        graphics.drawRect(Mouse.getPos().x - 812, Mouse.getPos().y, 800, 1); // left x axis line Stroke
+        graphics.fillOval(Mouse.getPos().x - 3, Mouse.getPos().y - 3, 7, 7); // Center dot stroke
+        graphics.setColor(scripts.SPXAIOPlanker.Constants.MOUSE_COLOR);
+        graphics.drawRect(Mouse.getPos().x - 12, Mouse.getPos().y - 12, 25, 25); // Square rectangle
+        graphics.drawRect(Mouse.getPos().x, Mouse.getPos().y - 512, 0, 500); // Top y axis Line
+        graphics.drawRect(Mouse.getPos().x, Mouse.getPos().y + 13, 0, 500); // Bottom y axis Line
+        graphics.drawRect(Mouse.getPos().x + 13, Mouse.getPos().y, 800, 0); // Right x axis line
+        graphics.drawRect(Mouse.getPos().x - 812, Mouse.getPos().y, 800, 0); // left x axis line
+        graphics.fillOval(Mouse.getPos().x - 2, Mouse.getPos().y - 2, 5, 5); // Center dot
+    }
+
+    @Override
+    public void paintMouseSpline(Graphics graphics, ArrayList<Point> arrayList) {
     }
 
 }
