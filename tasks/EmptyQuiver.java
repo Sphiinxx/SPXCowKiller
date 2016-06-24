d07b009bf7f695309af91803dfb7fd85ee3128a1
Sphiinx <deathbyman33@yahoo.com>
2016-06-24 10:59:39 -0700

    Non Version Update
    - N/A

--- tasks/EmptyQuiver.java
+++ tasks/EmptyQuiver.java
@@ -7,8 +7,8 @@ import org.tribot.api2007.*;
 import scripts.SPXCowKiller.data.Constants;
 import scripts.SPXCowKiller.data.Vars;
 import scripts.SPXCowKiller.framework.Task;
-import TribotAPI.game.combat.Combat07;
-import TribotAPI.game.game.Game07;
+import scripts.TribotAPI.game.combat.Combat07;
+import scripts.TribotAPI.game.game.Game07;
 
 /**
  * Created by Sphiinx on 1/20/2016.
